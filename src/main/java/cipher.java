a63fc69d798792f4dbe6fc5c7ac41f20e39262dc
Moringa School Access <wambuimbeatrice@gmail.com>
2020-10-12 21:53:56 +0300

    creating a ceasar cipher

---
+++ src/main/java/cipher.java
@@ -0,0 +1,33 @@
+import static java.lang.Character.isLowerCase;
+import static java.lang.Character.isUpperCase;
+
+public class cipher {
+    private static final int ALPHABET_SIZE = 26;
+    public String caesarcipher(String message, int rotateBy) {
+        rotateBy %= ALPHABET_SIZE;
+        char [] chars = message.toCharArray();
+        rotate(chars, rotateBy);
+        return new String(chars);
+    }
+
+    private void rotate(char[] chars, int rotateBy) {
+       for (int i = 0; i < chars.length; ++i){
+           if (isLowerCase(chars[i])){
+               chars[i] = rotateChar( chars[i], rotateBy, 'a', 'z');
+           }else if (isUpperCase(chars[i])){
+               chars[i] = rotateChar(chars[i], rotateBy, 'A', 'Z');
+           }
+       }
+    }
+    private char rotateChar (char c, int rotateBy, char firstChar, char lastChar){
+        c += rotateBy;
+        if (c < firstChar) {
+            return (char) (c + ALPHABET_SIZE);
+        }if (c > lastChar) {
+            return (char) (c - ALPHABET_SIZE);
+        }
+        return c;
+    }
+
+
+}
