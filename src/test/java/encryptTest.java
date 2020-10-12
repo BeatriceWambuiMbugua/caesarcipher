9700787d46f104bc3dc4c90aa5084b60f916f106
Moringa School Access <wambuimbeatrice@gmail.com>
2020-10-12 14:18:00 +0300

    Initial commit

---
+++ src/test/java/encryptTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class encryptTest {
+    private encrypt caesar = new encrypt();
+
+    @Test
+    public void name() {
+    }
+}
